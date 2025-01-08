e073a3681e0204339c1f1ac08b52118cec90bb53
eyefory <k.charantej7793@gmail.com>
2025-01-08 16:00:32 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ loop = asyncio.get_event_loop()
 
 async def start():
     print('\n')
-    print('Initalizing Tech VJ Bot')
+    print('MonkeyDluffyXbot')
     bot_info = await StreamBot.get_me()
     StreamBot.username = bot_info.username
     await initialize_clients()
