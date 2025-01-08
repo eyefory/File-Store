303c84189e1b5b4ef68f8c0f224f80384e0ecb7d
eyefory <k.charantej7793@gmail.com>
2025-01-08 15:18:28 +0530

    broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 from plugins.dbusers import db
 from pyrogram import Client, filters
@@ -10,18 +6,8 @@ import asyncio
 import datetime
 import time
 import logging
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 async def broadcast_messages(user_id, message):
     try:
         await message.copy(chat_id=user_id)
@@ -43,11 +29,6 @@ async def broadcast_messages(user_id, message):
     except Exception as e:
         return False, "Error"
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
 async def verupikkals(bot, message):
     users = await db.get_all_users()
@@ -61,11 +42,6 @@ async def verupikkals(bot, message):
     blocked = 0
     deleted = 0
     failed =0
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
     success = 0
     async for user in users:
         if 'id' in user:
@@ -83,7 +59,7 @@ async def verupikkals(bot, message):
             if not done % 20:
                 await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
         else:
-            # Handle the case where 'id' key is missing in the user dictionary
+        
             done += 1
             failed += 1
             if not done % 20:
@@ -91,8 +67,3 @@ async def verupikkals(bot, message):
     
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
